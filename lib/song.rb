0c2aee7c9d7989476ea7c4b45792f26b850eb70d
morgancollado <Morgan.collado@gmail.com>
2020-05-30 14:24:50 -0500

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,20 @@
+class Song
+
+    attr_accessor :name, :artist
+    @@all = []
+    def initialize(name)
+        @name = name
+        @@all << self
+    end 
+
+    def self.all
+        @@song
+    end 
+
+    def self.new_by_filename(filename)
+        song = self.new
+        song.name = filename.split(" - ")[1]
+        song.artist
+        song
+    end 
+end
