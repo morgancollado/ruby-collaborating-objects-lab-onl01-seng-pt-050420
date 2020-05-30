a139e514f28350c8f15d3674e172c84b15590b6a
Morgan Collado <morgan.collado@gmail.com>
2020-05-30 01:14:19 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,26 @@
+class Song 
+  attr_accessor :name, :artist
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    save
+  end 
+  
+  def save
+    @@all << self
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def self.new_by_filename(filename)
+    song = self.new 
+    song.name = filename.split(" - ")[1]
+    song 
+  end 
+    
+   
+end 
